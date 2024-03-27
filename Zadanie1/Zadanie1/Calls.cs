a3bb24030f6093e893602aa0b3a5dbb4628ca5f5
Akirochka <90083910+kurumikeur@users.noreply.github.com>
2024-03-27 23:02:12 +0300

    Initial commit

---
+++ Zadanie1/Zadanie1/Calls.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Dynamic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zadanie1
+{
+    public class Calls
+    {
+        static HttpClient client = new HttpClient();
+        private Task[] tasks = new Task[3];
+
+        public static async Task Sendrequest(string destination)
+        {
+            Stopwatch timer = Stopwatch.StartNew();
+            using HttpRequestMessage request = new HttpRequestMessage(HttpMethod.Get, destination);
+            using HttpResponseMessage response = await client.SendAsync(request);
+            timer.Stop();
+            Console.WriteLine(response.StatusCode.ToString());
+            Console.WriteLine("Got response in: {0} ms", timer.Elapsed.Milliseconds);
+        }
+
+    }
+}
---
+++ Zadanie1/Zadanie1/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+using zadanie1;
+
+Calls.Sendrequest("https://Google.com/");
+Calls.Sendrequest("https://yandex.ru");
+
+while (true)
+{
+    Thread.Sleep(1000);
+}
